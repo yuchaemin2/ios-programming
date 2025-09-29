c4dc18381e09d0ad3faac4242b1bc13c3342b81a
yuchaemin2 <kusuri94@gmail.com>
2025-09-29 14:45:10 +0900

    Feat:LoginView, MainView 연결

--- Test02/Test02/LoginView.swift
+++ Test02/Test02/LoginView.swift
@@ -18,34 +18,61 @@ struct LoginView: View {
     var body: some View {
         // UI 추가
         // dpi
-        VStack{
-            HStack {
-                Text("ID")
-                TextField("ID 입력", text: $id).onChange(of: id) { oldValue, newValue in
-                    print("\(oldValue) \(newValue)")
+        NavigationView {
+            VStack{
+                HStack {
+                    Text("ID")
+                    TextField("ID 입력", text: $id).onChange(of: id) { oldValue, newValue in
+                        print("\(oldValue) \(newValue)")
+                    }
                 }
-            }
-            HStack{
-                Text("PWD")
-                TextField("PWD 입력", text: $pwd)
-            }
-            
-            Button("로그인") {
-                print("로그인")
-                var a=10
-                let b=20
-                var c:String="aaaaa"
+                HStack{
+                    Text("PWD")
+                    TextField("PWD 입력", text: $pwd)
+                }
                 
-                // print("a + b = \(a+b) \(c)"+c)
-                print("id : \(id)")
-                print("pwd : \(pwd)")
-                if id.count < 4{
-                    print("ID를 입력해 주세요")
+                Button("로그인") {
+                    print("로그인")
+                    var a=10
+                    let b=20
+                    var c:String="aaaaa"
+                    
+                    // print("a + b = \(a+b) \(c)"+c)
+                    print("id : \(id)")
+                    print("pwd : \(pwd)")
+                    if id.count < 4 {
+                        print("ID를 입력해 주세요")
+                    }
+                    
+                    guard let url=URL(string: "http://localhost/login.php")
+                    else {
+                        print("url error")
+                        return
+                    }
+                    let body = "id=\(id)&pwd=\(pwd)"
+                    let encodedData=body.data(using: String.Encoding.utf8)
+                    
+                    var request=URLRequest(url:url)
+                    request.httpMethod="POST"
+                    request.httpBody=encodedData
+                    
+                    URLSession.shared.dataTask(with: request){
+                        (data,response,error) in
+                        if let error = error{
+                            print(error)
+                            return
+                        }
+                        guard let data=data else{
+                            return
+                        }
+                        
+           
+                    }.resume()
                 }
+                NavigationLink(destination: MainView()) {Text("로그인")}
             }
-            
+            .padding(.horizontal, 30.0)
         }
-        .padding(.horizontal, 30.0)
         
     }
 }
---
+++ Test02/Test02/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  Test02
+//
+//  Created by mac25 on 9/29/25.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text("Main View")
+    }
+}
+
+#Preview {
+    MainView()
+}
